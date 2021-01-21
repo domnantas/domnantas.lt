356d9f610bedcf7e423afa76cb5644e735efa073
domnantas <domantas@zyro.com>
2021-01-21 03:34:12 +0200

    Add styles and 404

---
+++ .vitepress/theme/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+	<h1>404</h1>
+	<blockquote>
+		OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code
+		monkeys at our headquarters are working VEWY HAWD to fix this!
+	</blockquote>
+	<a :href="$site.base" aria-label="go to home">Go to horny jail</a>
+</template>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,8 +1,10 @@
+import './styles/vars.css';
 import Layout from './Layout.vue'
+import NotFound from './NotFound.vue'
 
 export default {
   Layout,
-  NotFound: () => 'custom 404', // <- this is a Vue 3 functional component
+  NotFound,
   enhanceApp({ app, router, siteData }) {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
