452c32fcaea07bd84dda245ce717744b120a52ee
domnantas <hello@domnantas.lt>
2023-02-17 01:58:12 +0200

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "plugin:astro/recommended",
+		"plugin:astro/jsx-a11y-recommended"
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+  ],
+}
