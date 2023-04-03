4ab853e1148471051170fa864b81ae65e4659b2b
domnantas <hello@domnantas.lt>
2023-04-03 21:21:03 +0300

    Remove vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
@@ -8,7 +7,4 @@ import solidJs from "@astrojs/solid-js";
 export default defineConfig({
 	site: "https://domnantas.lt",
 	integrations: [solidJs()],
-	adapter: vercel({
-    analytics: true
-  })
 });
